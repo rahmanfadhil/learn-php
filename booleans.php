162efb0f0293843e698e737701f9752e6c06dcd0
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-06-20 21:09:13 +0700

    Learn booleans

---
+++ booleans.php
@@ -0,0 +1,29 @@
+<?php
+
+  // echo true; // returns "1"
+  // echo false; // returns ""
+
+  // Numbers
+  // echo 5 < 10;
+  // echo 5 > 10;
+  // echo 5 == 10;
+  // echo 10 == 10;
+  // echo 5 != 10;
+  // echo 5 <= 5;
+  // echo 5 >= 5;
+
+  // Strings
+  // echo 'shaun' < 'yoshi';
+  // echo 'shaun' > 'yoshi';
+  // echo 'shaun' > 'Yoshi';
+  // echo 'mario' == 'mario';
+  // echo 'mario' == 'Mario';
+
+  // Loose vs strict
+  // echo 5 == '5';
+  // echo 5 === '5';
+  // echo 5 === 5;
+  // echo true == "1";
+  // echo false == "";
+  // echo true === "1";
+  // echo false === "";
