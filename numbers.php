e4c868d71844bd02dc7a288896c8a346a117a954
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-06-20 16:55:33 +0700

    Learn numbers

---
+++ numbers.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Learn PHP Numbers</title>
+</head>
+<body>
+  <?php
+    $radius = 25;
+    $pi = 3.14;
+
+    // Basics: +, -, *, /, **
+    // echo $pi * $radius ** 2;
+
+    // order of operations ( B I D M A S )
+    // echo 2 * ( 4 + 9 ) / 3;
+
+    // Increment and decrement operators
+    // $radius--;
+    // echo $radius;
+
+    // Shorthand operators
+    // $age = 20;
+    // $age += 10;
+    // $age *= 2;
+    // echo $age;
+
+    // Number functions
+    // echo floor($pi);
+    // echo ceil($pi);
+    // echo pi();
+  ?>
+</body>
+</html>
