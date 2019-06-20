13f1eb852b6960fe4b9e9e60c065f49d49e49895
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-06-20 19:45:53 +0700

    Learn arrays

---
+++ arrays.php
@@ -0,0 +1,42 @@
+<?php
+    $peopleOne = ["hello", "world", "there"];
+    $peopleTwo = array("hello", "world", "there");
+    $ages = [20, 30, 40, 50];
+
+    // echo $peopleOne[0];
+    // echo $peopleTwo[0];
+
+    // Set item in array
+    // $ages[1] = 31;
+    // print_r($ages);
+    
+    // Add item to array
+    // $ages[] = 60;
+    // array_push($ages, 70);
+    // print_r($ages);
+
+    // Count aray length
+    // echo count($ages);
+
+    // Merge two (or more) arrays
+    // $peopleThree = array_merge($peopleOne, $peopleTwo);
+    // print_r($peopleThree);
+
+    // Associative array (key & value pairs)
+    // $ninjasOne = [
+    //   'shaun' => 'black',
+    //   'mario' => 'orange',
+    //   'luigi' => 'brown'
+    // ];
+    // echo $ninjasOne['mario'];
+    // $ninjasTwo = array(
+    //   'bowser' => 'green',
+    //   'peach' => 'yellow'
+    // );
+    // echo $ninjasTwo['bowser'];
+    // $ninjasTwo['toad'] = 'pink';
+    // print_r($ninjasTwo);
+    // echo count($ninjasTwo);
+    // $ninjasThree = array_merge($ninjasOne, $ninjasTwo);
+    // print_r($ninjasThree);
+    
