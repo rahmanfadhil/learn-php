3a1c322107bc116679b1dc4534cd32d36f73aa35
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-06-21 08:56:58 +0700

    Learn include and require

---
+++ include.php
@@ -0,0 +1,26 @@
+<?php
+
+  // include 'ninjas.php';
+  // require 'ninjas.php';
+
+  require 'ninjas.php';
+  include 'ninjas.php';
+
+  echo 'end of php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Learn Include & Require PHP</title>
+</head>
+<body>
+  <?php include 'content.php'; ?>
+  <?php include 'content.php'; ?>
+  <?php include 'content.php'; ?>
+</body>
+</html>
