1f1b077369af98f27cf8ca2f6878fa8adf751b71
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-06-25 16:51:44 +0700

    Learn php sessions

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
   define('NAME', "Fadhil");
   $age = 15;
+
+  session_start();
+
+  if ($_SERVER['QUERY_STRING'] == 'noname') {
+    unset($_SESSION['name']);
+  }
+
+  $name = $_SESSION['name'] ?? 'blahblah';
+
 ?>
 
 <!DOCTYPE html>
@@ -11,5 +20,6 @@
 <body>
   <h1>Hello, <?php echo NAME ?>!</h1>
   <p>You are <?php echo $age ?> years old</p>
+  <p>Session name: <?= $name ?></p>
 </body>
 </html>
