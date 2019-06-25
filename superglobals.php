99ceed8ac9246aaf880560b8a9f8769406224350
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-06-25 16:37:54 +0700

    Learn superglobals

---
+++ superglobals.php
@@ -0,0 +1,6 @@
+<?php
+
+  echo $_SERVER['SERVER_NAME'] . '<br />';
+  echo $_SERVER['REQUEST_METHOD'] . '<br />';
+  echo $_SERVER['SCRIPT_FILENAME'] . '<br />';
+  echo $_SERVER['PHP_SELF'] . '<br />';
