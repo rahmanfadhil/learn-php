f5e6a142efce55a0971ccbd5e53b2e913e097db5
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-06-25 17:04:52 +0700

    Learn php sessions

--- index.php
+++ index.php
@@ -8,7 +8,11 @@
     unset($_SESSION['name']);
   }
 
+  // Get session
   $name = $_SESSION['name'] ?? 'blahblah';
+
+  // Get cookie
+  $gender = $_COOKIE['gender'] ?? 'Unknown';
 
 ?>
 
@@ -21,5 +25,6 @@
   <h1>Hello, <?php echo NAME ?>!</h1>
   <p>You are <?php echo $age ?> years old</p>
   <p>Session name: <?= $name ?></p>
+  <p>Cookie gender: <?= $gender ?></p>
 </body>
 </html>
--- sessions.php
+++ sessions.php
@@ -1,6 +1,10 @@
 <?php
 
   if(isset($_POST['submit'])) {
+    // Cookies
+    setcookie('gender', $_POST['gender'], time() + 86400);
+
+    // Sessions
     session_start();
     $_SESSION['name'] = $_POST['name'];
     header('Location: index.php');
@@ -19,6 +23,10 @@
 <body>
   <form action="<?= $_SERVER['PHP_SELF']?>" method="POST">
     <input type="text" name="name">
+    <select name="gender">
+      <option value="male">Male</option>
+      <option value="female">Female</option>
+    </select>
     <input type="submit" name="submit" value="submit">
   </form>
 </body>
